0a9fc86b92f1dfd15019955fe8f29d7a32c6524b
jigargc <98375228+jigargc@users.noreply.github.com>
2022-12-18 17:09:56 -0500

    change password

--- routes/changePassword.js
+++ routes/changePassword.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {authMiddleware} = require("../authMiddleware/authMiddleware");
 const {passXss} = require("../helpers");
 const {changeUserPassword, changeRestaurantPassword} = require("../data/changePassword");
+const {findRestaurantByResId} = require("../data/restaurants");
 const router = express.Router();
 router.use(authMiddleware);
 router.route('/').get((req, res) => {
@@ -9,20 +10,30 @@ router.route('/').get((req, res) => {
 }).post(async (req, res) => {
     passXss(req.body);
     try {
+
+        let rest;
         const {prevPassword, password} = req.body;
         if (req._user.isUser) {
             await changeUserPassword(req._user.id, prevPassword, password);
         } else {
+            rest = await findRestaurantByResId(req._user.id);
             await changeRestaurantPassword(req._user.id, prevPassword, password);
         }
         res.render('changePassword', {
             layout: req._user.isUser ? 'user' : 'restaurant',
             title: "Change Password",
-            success: "Password successfully changed"
+            success: "Password successfully changed",
+            rest
         });
     } catch (e) {
+        let rest;
+        if (!req._user.isUser) {
+            rest = await findRestaurantByResId(req._user.id);
+            await changeRestaurantPassword(req._user.id, prevPassword, password);
+        }
         res.status(400).render('changePassword', {
-            layout: req._user.isUser ? 'user' : 'restaurant', title: "Change Password", error: e
+            layout: req._user.isUser ? 'user' : 'restaurant', title: "Change Password", error: e,
+            rest
         });
     }
 })
